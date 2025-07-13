33d3bf86eccb351389f0cbbdd1ad65f2cd34e1c3
andresfeliper3 <andresfeliper.2001@gmail.com>
2025-07-13 10:16:26 -0500

    swagger description in product route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -3,8 +3,68 @@ import { getAllProducts, createProduct, deleteProduct } from '../controllers/pro
 
 const router = Router();
 
+/**
+ * @openapi
+ * /products:
+ *   get:
+ *     summary: Retrieve all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of products
+ */
 router.get('/products', getAllProducts);
+
+
+/**
+ * @openapi
+ * /products:
+ *   post:
+ *     summary: Add a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       description: Product object to create
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *             required:
+ *               - name
+ *               - description
+ *               - price
+ *     responses:
+ *       201:
+ *         description: Product created
+ */
 router.post('/products', createProduct);
+
+
+/**
+ * @openapi
+ * /products/{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Product deleted
+ *       404:
+ *         description: Product not found
+ */
 router.delete('/products/:id', deleteProduct);
 
 export default router;
