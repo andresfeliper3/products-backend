e1dc2cd7ea8cbe1fb24f39fd0f69c1ee3ac2c094
andresfeliper3 <andresfeliper.2001@gmail.com>
2025-07-13 09:38:47 -0500

    Product model file using Sequelize

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,1 +1,37 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/database';
 
+export class Product extends Model {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+  public price!: number;
+}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: new DataTypes.STRING(255),
+      allowNull: false,
+    },
+    description: {
+      type: new DataTypes.TEXT,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Product',
+    tableName: 'products',
+    timestamps: false,
+  }
+);
